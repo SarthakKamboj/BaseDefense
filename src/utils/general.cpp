4efee7d1c3ccf50c9092f468c5aaa362e67d99e7
SarthakKamboj <sarthakkamboj22@gmail.com>
2024-01-12 00:48:37 -0800

    created a general utils file as well

---
+++ src/utils/general.cpp
@@ -0,0 +1,5 @@
+#include "general.h"
+
+glm::vec3 create_color(float r, float g, float b) {
+    return glm::vec3(r, g, b) / 255.f;
+}
---
+++ src/utils/general.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "glm/glm.hpp"
+
+glm::vec3 create_color(float r, float g, float b);
